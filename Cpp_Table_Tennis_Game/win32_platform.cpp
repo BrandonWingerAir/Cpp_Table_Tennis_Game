d2e7b238f27c3acdc835526a25891c03b9c2d7b7
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-03-05 01:01:35 -0500

    Add register class & create window functions

--- Cpp_Table_Tennis_Game/win32_platform.cpp
+++ Cpp_Table_Tennis_Game/win32_platform.cpp
@@ -12,6 +12,20 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLin
 	window_class.lpfnWndProc = window_callback;
 
 	// Register Class
+	RegisterClass(&window_class);
 
 	// Create Window
+	CreateWindow(
+		window_class.lpszClassName, 
+		"Table Tennis", 
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE, 
+		CW_USEDEFAULT, 
+		CW_USEDEFAULT, 
+		1280, 
+		720, 
+		0, 
+		0, 
+		hInstance, 
+		0
+	);
 }
