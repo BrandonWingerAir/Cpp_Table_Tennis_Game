492b964f0895435b7932d5a0c3b300983c2727fe
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-03-04 21:29:50 -0500

    Create Window class and callback function

--- Cpp_Table_Tennis_Game/win32_platform.cpp
+++ Cpp_Table_Tennis_Game/win32_platform.cpp
@@ -1,5 +1,17 @@
 #include <windows.h>
 
-int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+LRESULT CALLBACK window_callback(HWND handleWindow, UINT systemMessage, WPARAM messageInfo, LPARAM additionalInfo) {
+	return DefWindowProc(handleWindow, systemMessage, messageInfo, additionalInfo);
+}
 
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nShowCmd) {
+	// Create Window Class
+	WNDCLASS window_class = {};
+	window_class.style = CS_HREDRAW | CS_VREDRAW;
+	window_class.lpszClassName = "Game Window Class";
+	window_class.lpfnWndProc = window_callback;
+
+	// Register Class
+
+	// Create Window
 }
